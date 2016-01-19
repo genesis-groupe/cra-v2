20ddbd9aa7e5b6a56736200a8dfe3a88f2fd4c95
leodagdag <leodagdag@gmail.com>
2016-01-19 19:48:52 +0100

    Fix Week 53

--- app/dto/WeekDTO.java
+++ app/dto/WeekDTO.java
@@ -40,12 +40,14 @@ public class WeekDTO {
 			for(Integer weekNumber : weeks.keySet()) {
 				result.add(new WeekDTO(weekNumber, weeks.get(weekNumber), missions, year, month));
 			}
-			if(month == DateTimeConstants.DECEMBER) {
+			if(month == DateTimeConstants.DECEMBER || month == DateTimeConstants.JANUARY) {
 				Collections.sort(result, new Comparator<WeekDTO>() {
 					@Override
 					public int compare(final WeekDTO w1, final WeekDTO w2) {
 						if(w1.number == 1) return 1;
 						else if(w2.number == 1) return -1;
+						else if(w1.number == 53) return -1;
+						else if(w2.number == 53) return -1;
 						else return w1.number.compareTo(w2.number);
 					}
 				});
